bc9d82ee1f0491aafc4a1568376334afa0f72184
Andres Monsalve <felipe20001monsalve@gmail.com>
2025-05-29 18:17:08 -0500

    configuración de contenedor padre con sus modulos y configuracion basica de Servicio Eureka

---
+++ microservice-gataway/src/main/java/com/microservice/gateway/microservice_gataway/MicroserviceGatawayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.gateway.microservice_gataway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatawayApplication.class, args);
+	}
+
+}
